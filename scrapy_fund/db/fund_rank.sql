bef3c45d1b07a8398dfacad91d1372508aa92dfc
kunjie <kunjie.zhang@corp.elong.com>
2017-09-13 16:41:15 +0800

    2017-09-13

--- scrapy_fund/db/fund_rank.sql
+++ scrapy_fund/db/fund_rank.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50709
  File Encoding         : utf-8
 
- Date: 08/04/2017 08:25:51 AM
+ Date: 09/13/2017 08:46:01 AM
 */
 
 SET NAMES utf8;
@@ -24,23 +24,24 @@ DROP TABLE IF EXISTS `fund_rank`;
 CREATE TABLE `fund_rank` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `fund_id` varchar(20) DEFAULT NULL,
-  `fund_name` varchar(50) DEFAULT NULL,
-  `fund_name_abbr` varchar(20) DEFAULT NULL,
-  `cal_date` date DEFAULT NULL,
-  `net_asset_value` decimal(10,4) DEFAULT NULL,
-  `accumulative` decimal(10,4) DEFAULT NULL,
-  `oneday` varchar(255) DEFAULT NULL,
-  `oneweek` varchar(255) DEFAULT NULL,
-  `onemonth` varchar(255) DEFAULT NULL,
-  `threemonth` varchar(255) DEFAULT NULL,
-  `sixmonth` varchar(255) DEFAULT NULL,
-  `oneyear` varchar(255) DEFAULT NULL,
-  `twoyear` varchar(255) DEFAULT NULL,
-  `threeyear` varchar(255) DEFAULT NULL,
-  `thisyear` varchar(255) DEFAULT NULL,
-  `setup` varchar(255) DEFAULT NULL,
-  `category` varchar(255) DEFAULT NULL,
+  `fund_name` varchar(100) DEFAULT NULL,
+  `fund_name_abbr` varchar(50) DEFAULT NULL,
+  `cal_date` varchar(50) DEFAULT NULL,
+  `net_asset_value` float(10,4) DEFAULT NULL,
+  `accumulative` float(10,4) DEFAULT NULL,
+  `oneday` float(20,4) DEFAULT NULL,
+  `oneweek` float(20,4) DEFAULT NULL,
+  `onemonth` float(20,4) DEFAULT NULL,
+  `threemonth` float(20,4) DEFAULT NULL,
+  `sixmonth` float(20,4) DEFAULT NULL,
+  `oneyear` float(20,4) DEFAULT NULL,
+  `twoyear` float(20,4) DEFAULT NULL,
+  `threeyear` float(20,4) DEFAULT NULL,
+  `thisyear` float(20,4) DEFAULT NULL,
+  `setup` float(20,4) DEFAULT NULL,
+  `category` varchar(20) DEFAULT NULL,
+  `score` float(20,4) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10106 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6160 DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
